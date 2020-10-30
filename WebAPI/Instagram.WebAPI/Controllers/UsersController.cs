fa9f727fb6c484496654fe852d8c56c18713b376
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2020-10-31 01:07:27 +0200

    Implement Authentication with JWT

--- WebAPI/Instagram.WebAPI/Controllers/UsersController.cs
+++ WebAPI/Instagram.WebAPI/Controllers/UsersController.cs
@@ -1,10 +1,15 @@
 ﻿using Instagram.WebAPI.Models;
-using Instagram.WebAPI.Services;
 using Instagram.WebAPI.ViewModels.Users;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Instagram.WebAPI.Controllers
@@ -14,10 +19,14 @@ namespace Instagram.WebAPI.Controllers
     public class UsersController : ControllerBase
     {
         private readonly UserManager<User> userManager;
+        private readonly SignInManager<User> signInManager;
+        private readonly IConfiguration configuration;
 
-        public UsersController(UserManager<User> userManager)
+        public UsersController(UserManager<User> userManager, SignInManager<User> signInManager, IConfiguration configuration)
         {
             this.userManager = userManager;
+            this.signInManager = signInManager;
+            this.configuration = configuration;
         }
 
         [HttpGet]
@@ -53,11 +62,42 @@ namespace Instagram.WebAPI.Controllers
         }
 
         [HttpPost]
-        public IActionResult Login(string username, string password)
+        public async Task<IActionResult> Login([FromBody] LoginInputModel loginInput)
         {
+            var result = await this.signInManager.PasswordSignInAsync(loginInput.Username, loginInput.Password, true, true);
 
+            if (!result.Succeeded)
+            {
+                return this.BadRequest();
+            }
 
-            return Ok();
+            var user = this.userManager.Users.FirstOrDefault(x => x.UserName == loginInput.Username);
+
+            return this.Ok(this.GenerateJwtToken(loginInput.Username, user));
+        }
+
+        private object GenerateJwtToken(string email, IdentityUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, email),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(ClaimTypes.NameIdentifier, user.Id)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtKey"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var expires = DateTime.Now.AddDays(Convert.ToDouble(configuration["JwtExpireDays"]));
+
+            var token = new JwtSecurityToken(
+                configuration["JwtIssuer"],
+                configuration["JwtIssuer"],
+                claims,
+                expires: expires,
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
