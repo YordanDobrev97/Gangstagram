aaf3d1131a111627d68c548844229efc1a95e764
Yordan Dobrev <42092212+YordanDobrev97@users.noreply.github.com>
2021-03-12 21:09:32 +0200

    Split of components. Improved code

--- client/src/Components/Home/index.jsx
+++ client/src/Components/Home/index.jsx
@@ -1,17 +1,14 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import {
     Link,
     Route,
     Switch,
     BrowserRouter as Router,
-    Redirect,
   } from "react-router-dom";
 
 import { Container } from '@material-ui/core';
-import { AppBar } from '@material-ui/core';
-import { Box } from '@material-ui/core'
-import { Button } from '@material-ui/core';
 
+import Navigation from '../Navigation/index';
 import Login from '../Login/index';
 import Register from "../Register/index";
 import Feeds from '../Feed/all';
@@ -24,53 +21,9 @@ import Cookies from "universal-cookie";
 const cookies = new Cookies();
 
 class Home extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            isLoggedIn: false,
-            isClickCreatePost: false,
-            isClickLogout:false,
-        };
-    }
-
     static contextType = UserContext
-
-    componentDidMount() {
-        this.setState({
-            isLoggedIn: this.context[0].isAuth,
-        })
-    }
     
-    logout = () => {
-        this.context[1]({
-            username: null,
-            isAuth: false
-        });
-
-        console.log(this.context);
-
-        document.cookie = 'userId' +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-        this.setState({
-            isClickLogout: true
-        })
-    }
-
-    createPost = () => {
-        this.setState({
-            isClickCreatePost: true
-        });
-    }
-
     render() {
-        if (this.state.isClickLogout) {
-            return (<Redirect to='/' />)
-        }
-
-        if (this.state.isClickCreatePost) {
-            return (<Redirect to='/createPost'/>)
-        }
-
         return (
             <Container maxWidth="lg">
                 <Router>
@@ -81,45 +34,7 @@ class Home extends Component {
                     <Link to="/feed/:id" />
                     <Link to="/createPost" />
 
-                    <AppBar display="flex" mb={4} position="static" color="transparent">
-                    
-                        <Box>
-                            <h1>
-                                <a href={this.context[0].isAuth ? '/feeds': '/'}>Gangstagram</a>
-                            </h1>
-                            {this.context[0].isAuth ? (
-                                <p>Hello, {this.context[0].username}</p>
-                            )  : (
-                                <React.Fragment></React.Fragment>
-                            )}
-                        </Box>
-                    
-
-                        {this.context[0].isAuth ? (
-                            <Box textAlign='center'>
-                                <Button onClick={this.createPost.bind(this)} style={{Width: '50px', marginLeft: '100px'}} variant="contained" color="primary" component="button">
-                                    Add Post
-                                </Button>
-
-                                <Button onClick={this.logout.bind(this)} style={{Width: '50px', float: 'right'}} variant="contained" color="secondary" component="button">
-                                    Logout
-                                </Button>
-                            </Box>
-                        ) : (
-                            <Fragment>
-                                <Box>
-                                    <Button fullWidth={false} size="medium" color="primary" href="/register">
-                                        Register
-                                    </Button>
-                                </Box>
-                                <Box>
-                                    <Button fullWidth={false} size="medium" color="primary" href="/login">
-                                        Login
-                                    </Button>
-                                </Box>
-                            </Fragment>
-                        )}
-                    </AppBar>
+                    <Navigation />
 
                     <Switch>
                         <Route path="/login">
--- client/src/Components/Navigation/index.js
+++ client/src/Components/Navigation/index.js
@@ -1,19 +1,111 @@
 import React, { Component, Fragment } from "react";
-import Search from "../Search/index";
+import { Redirect } from "react-router-dom";
+
+import { AppBar, Box, Button } from "@material-ui/core";
+import UserContext from "../../UserContext";
 
 class Navigation extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isClickCreatePost: false,
+      isClickLogout: false,
+    };
+  }
+  static contextType = UserContext;
+
+  logout = () => {
+    this.context[1]({
+      username: null,
+      isAuth: false,
+    });
+
+    console.log(this.context);
+
+    document.cookie =
+      "userId" + "=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+    this.setState({
+      isClickLogout: true,
+    });
+  };
+
+  createPost = () => {
+    this.setState({
+      isClickCreatePost: true,
+    });
+  };
+
   render() {
+    if (this.state.isClickLogout) {
+      return <Redirect to="/" />;
+    }
+
+    if (this.state.isClickCreatePost) {
+      return <Redirect to="/createPost" />;
+    }
+
     return (
       <Fragment>
-        <nav className="navbar navbar-dark bg-dark navbar-toggler">
-          <div>
-            <img
-              className="w-50"
-              src="https://raw.githubusercontent.com/nomadcoders/vietgram/master/images/logo.png"
-            />
-          </div>
-          <Search message="Search users" />
-        </nav>
+        <AppBar display="flex" mb={4} position="static" color="transparent">
+          <Box>
+            <h1>
+              <a href={this.context[0].isAuth ? "/feeds" : "/"}>Gangstagram</a>
+            </h1>
+            {this.context[0].isAuth ? (
+              <p>Hello, {this.context[0].username}</p>
+            ) : (
+              <React.Fragment></React.Fragment>
+            )}
+          </Box>
+
+          {this.context[0].isAuth ? (
+            <Box textAlign="center">
+              <Button
+                onClick={this.createPost.bind(this)}
+                style={{ Width: "50px", marginLeft: "100px" }}
+                variant="contained"
+                color="primary"
+                component="button"
+              >
+                Add Post
+              </Button>
+
+              <Button
+                onClick={this.logout.bind(this)}
+                style={{ Width: "50px", float: "right" }}
+                variant="contained"
+                color="secondary"
+                component="button"
+              >
+                Logout
+              </Button>
+            </Box>
+          ) : (
+            <Fragment>
+              <Box>
+                <Button
+                  fullWidth={false}
+                  size="medium"
+                  color="primary"
+                  href="/register"
+                >
+                  Register
+                </Button>
+              </Box>
+              <Box>
+                <Button
+                  fullWidth={false}
+                  size="medium"
+                  color="primary"
+                  href="/login"
+                >
+                  Login
+                </Button>
+              </Box>
+            </Fragment>
+          )}
+        </AppBar>
       </Fragment>
     );
   }
